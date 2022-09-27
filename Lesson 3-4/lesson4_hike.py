643fda43040684d49715bc242cca42a79422a8d2
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-09-27 17:27:10 +0200

    -

--- Lesson 3-4/lesson4_hike.py
+++ Lesson 3-4/lesson4_hike.py
@@ -1,5 +1,43 @@
 path = "Hike.txt"
 
+def getDistance(x0:float, y0:float, x1:float, y1:float) -> float:
+    dx = x1 - x0
+    dy = y1 - y0
+    return (dx**2 + dy**2)**0.5
+
+def get3DDistance(x0:float, y0:float, z0:float, x1:float, y1:float, z1:float) -> float:
+    dx = x1 - x0
+    dy = y1 - y0
+    dz = z1 - z0
+    return (dx**2 + dy**2 + dz**2)**0.5
+
+def getCoordingLists(path:str) -> tuple:
+    X, Y, Z = [], [], []
+    with open(path) as file:
+        for row in file:
+            sx, sy, sz = row.split(' ')
+            X.append(float(sx))
+            Y.append(float(sy))
+            Z.append(float(sz))
+    return (X, Y, Z)
+
+def getLineStringLength2D(X: list, Y: list) ->float:
+    s = 0.0
+    for i in range(0, len(X)-1):
+        s += getDistance(X[i], Y[i], X[i+1], Y[i+1])
+    return s
+def getLineStringLength3D(X: list, Y: list, Z:list) ->float:
+    s = 0.0
+    for i in range(0, len(X)-1):
+        s += getDistance(X[i], Y[i], X[i+1], Y[i+1], Z[i], Z[i+1])
+    return s
+
+X, Y, Z = getCoordingLists(path)
+print(getLineStringLength2D(X, Y))
+print(getLineStringLength3D(X, Y, Z))
+
+
+
 #read from file the 3 coordinates of each point
 with open(path, 'r') as f:
     data = f.readlines()
